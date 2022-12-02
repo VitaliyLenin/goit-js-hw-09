96b86a0369a9644146ebf3b7a1a14d7993be1d11
VitaliyLenin <vitaliylenin77@gmail.com>
2022-12-02 20:00:50 +0200

    Update 02-timer.js

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -10,12 +10,13 @@ const refs = {
   secondsValue: document.querySelector('span[data-seconds]'),
 };
 
+refs.startBtn.addEventListener('click', handleStartClick);
 refs.startBtn.setAttribute('disabled', true);
 
 let ms = 0;
 let dateNow = 0;
 let userDate = 0;
-let timerId = 0;
+let timerId = null;
 
 const options = {
   enableTime: true,
@@ -32,8 +33,51 @@ const options = {
     }
 
     refs.startBtn.removeAttribute('disabled');
+    Notiflix.Notify.info('Натисніть "start" для початку відліку');
     console.log(selectedDates[0]);
   },
 };
 
 flatpickr('input[id=datetime-picker]', options);
+
+function convertMs(ms) {
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  const days = addLeadingZero(Math.floor(ms / day));
+  const hours = addLeadingZero(Math.floor((ms % day) / hour));
+  const minutes = addLeadingZero(Math.floor(((ms % day) % hour) / minute));
+  const seconds = addLeadingZero(
+    Math.floor((((ms % day) % hour) % minute) / second)
+  );
+
+  return { days, hours, minutes, seconds };
+}
+
+function onStartCounter(ms) {
+  dateNow = Date.now();
+  ms = userDate - dateNow;
+
+  let currentValue = convertMs(ms);
+
+  refs.daysValue.textContent = currentValue.days;
+  refs.hoursValue.textContent = currentValue.hours;
+  refs.minutesValue.textContent = currentValue.minutes;
+  refs.secondsValue.textContent = currentValue.seconds;
+
+  if (ms < 1000) {
+    clearInterval(timerId);
+  }
+}
+
+function handleStartClick() {
+  timerId = setInterval(onStartCounter, 1000);
+  Notiflix.Notify.success('Відлік розпочато');
+  refs.startBtn.setAttribute('disabled', true);
+}
+
+function addLeadingZero(value) {
+  return String(value).padStart(2, '0');
+}
