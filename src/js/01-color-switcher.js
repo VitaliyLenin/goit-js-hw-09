5d6ea58a11b550bcb680dddda735bcc2d0f08ac5
VitaliyLenin <vitaliylenin77@gmail.com>
2022-12-02 14:20:14 +0200

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,29 @@
+const refs = {
+  startBtn: document.querySelector('button[data-start]'),
+  stopBtn: document.querySelector('button[data-stop]'),
+};
 
+refs.startBtn.addEventListener('click', handleStartClick);
+refs.stopBtn.addEventListener('click', handleStopClick);
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function onChangeBodyColor() {
+  document.body.style.backgroundColor = getRandomHexColor();
+}
+
+let timerId = null;
+
+function handleStartClick() {
+  timerId = setInterval(onChangeBodyColor, 1000);
+  refs.startBtn.setAttribute('disabled', true);
+}
+
+function handleStopClick() {
+  clearInterval(timerId);
+  if (handleStopClick) {
+    refs.startBtn.removeAttribute('disabled');
+  }
+}
